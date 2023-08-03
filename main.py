fff28bec1136fe6df0a67d142d5c70aec91580c5
pasha <pashadubov2@gmail.com>
2023-08-03 17:27:47 +0300

    1.скачали пай гейм 2.создали экран 3.научились закрывать игру

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-####
+import pygame
+import sys
+
+
+pygame.init()
+
+
+
+s_size = 480,700
+
+bg_color = 254,254,254
+
+screen = pygame.display.set_mode(s_size)
+
+
+while True :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(bg_color)
+    pygame.display.update()
