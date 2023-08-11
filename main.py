a78ffefd8c6af562a5746d9bbb9c248a1fd30f5b
pasha <pashadubov2@gmail.com>
2023-08-11 17:38:00 +0300

    1.Создали класс платформа 2.Нарисовали платформы 3.Начали делать логику платформы 9 3/4

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+from platform import Platform
 
 
 pygame.init()
@@ -9,9 +11,18 @@ class Main:
         self.s_size = 480, 700
         self.screen = pygame.display.set_mode(self.s_size)
         self.bg_image = pygame.image.load("images/fon_dudljump.png")
+        self.platforms = [Platform(self.s_size[0] / 2 - 45, self.s_size[1] - 30)]
+        self.create_platform()
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.bg_image, self.bg_image.get_rect())
+        for platform in  self.platforms:
+            platform.draw(self.screen)
+    def create_platform(self):
+        y_p = self.platforms[0].rect.y - 120
+        for platf in range (5):
+            self.platforms.append(Platform(random.randint(0, self.s_size[0] - 90), y_p))
+            y_p = self.platforms[-1].rect.y - 120
 
 
 main  = Main()
---
+++ platform.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Platform:
+    def __init__(self, x, y):
+        self.color = (0, 0, 0)
+        self.size = 90, 10
+        self.speed = 0
+        self.rect = pygame.Rect(x, y, self.size[0], self.size[1])
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
