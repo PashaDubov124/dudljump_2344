c7aa97f185cb8575ad6c51fe77e93e5c99680d00
pasha <pashadubov2@gmail.com>
2023-08-24 17:30:09 +0300

    1.Сделали логику Dudlik  и сделали управление 9 3/4

--- dudlik.py
+++ dudlik.py
@@ -1,10 +1,31 @@
 import pygame
 
+
 class Dudlik:
-    def __init__(self, x, y):
+    def __init__(self, x, y, s_size):
         self.x, self.y = x, y
-        self.image = pygame.image.load('images/dudlik.png')
+        self.image = pygame.image.load('images/666.png')
         self.rect = self.image.get_rect()
         self.rect.center = x, y
+        self.speed_x = 0
+        self.speed = 4
+        self.s_size = s_size
+        self.speed_y = 0
+
+    def logic(self):
+        if self.rect.left > self.s_size[0]:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = self.s_size[0]
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def jump(self, platforms):
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.rect.bottom == platform.rect.top:
+                    self.speed_y += 10
+
+    def move(self):
+        self.rect.x += self.speed_x
+        self.rect.y -= self.speed_y
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dudlik import Dudlik
 
 pygame.init()
 
+
 class Main:
     def __init__(self):
         self.s_size = 480, 700
@@ -15,47 +16,55 @@ class Main:
         self.create_platform()
         self.clock = pygame.time.Clock()
         self.fon = Back_ground(self.s_size)
-        self.dudlik = Dudlik(self.s_size[0] / 2, self.s_size[1] - 70)
+        self.dudlik = Dudlik(self.s_size[0] / 2, self.s_size[1] - 70, self.s_size)
+
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.fon.draw(self.screen)
-        for platform in  self.platforms:
+        for platform in self.platforms:
             platform.draw(self.screen)
         self.dudlik.draw(self.screen)
+
     def create_platform(self):
         y_p = self.platforms[0].rect.y - 120
-        for platf in range (6):
+        for platf in range(6):
             self.platforms.append(Platform(random.randint(0, self.s_size[0] - 90), y_p))
             y_p = self.platforms[-1].rect.y - 120
+
     def update(self):
         for platform in self.platforms:
             platform.move()
         self.platform_logic()
         self.fon.move()
         self.fon.logic()
+        self.dudlik.move()
+        self.dudlik.logic()
+
     def platform_logic(self):
         for platform in self.platforms:
             if platform.rect.top >= self.s_size[1] + 50:
                 self.platforms.remove(platform)
                 y_p = self.platforms[-1].rect.y - 120
-                self.platforms.append(Platform(random.randint(0, self.s_size[0] - 90), y_p))
+                self.platforms.append(Platform(random.randint(- 45, self.s_size[0] - 45), y_p))
 
 
+main = Main()
 
-main  = Main()
-
-
-
-
-
-
-
-while True :
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                main.dudlik.speed_x = -main.dudlik.speed
+            if event.key == pygame.K_RIGHT:
+                main.dudlik.speed_x = main.dudlik.speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                main.dudlik.speed_x = 0
+            if event.key == pygame.K_RIGHT:
+                main.dudlik.speed_x = 0
     main.draw()
     main.update()
     pygame.display.update()
